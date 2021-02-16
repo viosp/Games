035add4ad37abb903ab04ef0cfa6cfed05c1cc6b
viosp <viv3e_xd@hotmail.com>
2021-02-16 11:53:05 +0200

    Snake

---
+++ snake/SnakeGame.java
@@ -0,0 +1,12 @@
+package com.codegym.games.snake;
+
+import com.codegym.engine.cell.Game;
+import com.codegym.engine.cell.*;
+
+public class SnakeGame extends Game {
+
+    public static void main(String[] args) {
+
+    }
+
+}
